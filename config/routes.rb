eb628d91dac287c28c05e8842a96084159e9983f
Paul Jamison <pbjamiso@uncc.edu>
2017-10-03 15:12:01 +0000

    Start of Chapter 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+Myrottenpotatoes::Application.routes.draw do
+  resources :movies
+  root :to => redirect('/movies')
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
